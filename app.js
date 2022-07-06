7111ec5eb6f7cbb82237a23dfeaf700cada52c18
Omar Vayani <vayaniumar@gmail.com>
2022-07-06 22:49:49 +0200

    finished project

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const display = document.querySelector("#display");
 const buttons = document.querySelectorAll("button");
 
-let value;
-
+let value = 0;
+display.innerHTML = value;
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
-    display.innerHTML =
-      button.innerHTML == "+"
-        ? (display.innerHTML += 1)
-        : button.innerHTML == "-"
-        ? (display.innerHTML -= 1)
-        : (display.innerHTML = "");
+    button.innerHTML == "+"
+      ? (value += 1)
+      : button.innerHTML == "-"
+      ? value == 0
+        ? (value = 0)
+        : (value -= 1)
+      : (value = 0);
+    display.innerHTML = value;
   });
 });
